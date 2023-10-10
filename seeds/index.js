a0131a4f87b5ce26d7824faa92412cf728c3f96c
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-10-09 22:16:28 -0300

    way behind on commits changed alot

---
+++ seeds/index.js
@@ -0,0 +1,30 @@
+const sequelize = require('../config/connection')
+const { Blog, Comment, User } = require('../models')
+
+
+const seedBlog = require('./blogData.json')
+const seedComment = require('./commentData.json')
+const seedUser = require('./userData.json')
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+
+    await User.bulkCreate(seedUser, {
+        individualHooks:true,
+        returning: true,
+    });
+
+    for (const blog of seedBlog) {
+        await Blog.create({
+          ...blog,
+          user_id: users[Math.floor(Math.random() * users.length)].id,
+        });
+      }
+
+    await Comment.bulkCreate(seedComment);
+
+    process.exit(0);
+
+};
+
+seedAll();
